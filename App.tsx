aaaf022ca200289dbd84498543da258543a2fc3f
Ivan Artamonov <i.o.artamonov@gmail.com>
2024-04-16 00:33:39 +0300

    Make dummy markup for books sliders

--- App.tsx
+++ App.tsx
@@ -65,23 +65,19 @@ type BooksWidgetProps = PropsWithChildren<{
 
 function BooksWidget({heading, books}: BooksWidgetProps): React.JSX.Element {
   return (
-    <View style={{padding: 10}}>
+    <View style={styles.booksWidget}>
       <Text style={styles.heading}>{heading}</Text>
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.booksWidget}>
+        contentContainerStyle={styles.bookList}>
         {books.map((book, index) => (
-          <View key={index} style={{width: 115}}>
+          <View key={index} style={styles.bookItem}>
             <Image
               source={{
                 uri: book.cover,
               }}
-              style={{
-                width: 220 / 2,
-                height: 320 / 2,
-                borderRadius: 5,
-              }}
+              style={styles.bookCover}
             />
             <Text>{book.title}</Text>
           </View>
@@ -165,9 +161,20 @@ const styles = StyleSheet.create({
     color: 'black',
   },
   booksWidget: {
+    padding: 10,
+  },
+  bookList: {
     flexDirection: 'row',
     paddingVertical: 10,
     gap: 10,
+  },
+  bookItem: {
+    width: 115,
+  },
+  bookCover: {
+    width: 220 / 2,
+    height: 320 / 2,
+    borderRadius: 5,
   },
   sectionContainer: {
     marginTop: 32,
