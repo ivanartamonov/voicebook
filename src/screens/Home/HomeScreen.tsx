65f3ec7f57d769d1ccf2880b2dcdc21d48d22e21
Ivan Artamonov <i.o.artamonov@gmail.com>
2024-04-17 15:26:16 +0300

    Create Screens and Components directories

---
+++ src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import BooksWidget from '../../components/BooksWidget/BooksWidget.tsx';
+
+function HomeScreen(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const books = [
+    {
+      title: 'Заборонена для мажора',
+      cover: 'https://st.booknet.ua/uploads/covers/220/1695712337_4.png',
+    },
+    {
+      title: 'За власним бажанням',
+      cover: 'https://st.booknet.ua/uploads/covers/220/1708788896_26.jpg',
+    },
+    {
+      title: 'Час пробачати',
+      cover: 'https://st.booknet.ua/uploads/covers/220/1697796419_56.jpg',
+    },
+    {
+      title: "П'янкий смак кохання",
+      cover: 'https://st.booknet.ua/uploads/covers/220/1673221872_64.jpg',
+    },
+    {
+      title: 'Залишся єдиним',
+      cover: 'https://st.booknet.ua/uploads/covers/220/1658075318_26.jpeg',
+    },
+  ];
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <BooksWidget heading="Popular books" books={books} />
+          <BooksWidget heading="Hot new releases" books={books} />
+          <BooksWidget heading="Bestsellers" books={books} />
+          <BooksWidget heading="Recently updated" books={books} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
